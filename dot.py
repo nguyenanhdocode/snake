39d17cc6c241d52a6e286a117cf4331e1ba050fd
Nguyen Anh Do <nguyenanhdorussian@gmail.com>
2021-04-02 09:12:55 +0700

    Add dot.py

---
+++ dot.py
@@ -0,0 +1,11 @@
+import pygame
+import config as cf
+
+class Dot:
+
+    def __init__(self, surface, x, y):
+        self.x, self.y = 0, 0
+        self.surface = surface
+    
+    def draw(self):
+        pygame.draw.rect(self.surface, cf.SNAKE_COLOR, pygame.Rect(self.x, self.y, cf.CELL_SIZE, cf.CELL_SIZE))
