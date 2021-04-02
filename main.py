0a2c9c5f9dfa43461d37d7236295f3f886210cca
Nguyen Anh Do <nguyenanhdorussian@gmail.com>
2021-04-02 10:04:41 +0700

    Modified main.py, snake.py, config.py

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ surface = pygame.display.set_mode((cf.WINDOW_WIDTH, cf.WINDOW_HEIGHT))
 # new snake
 snake = Snake(surface)
 
+# key status
+
+
 while (True):
+
+    pygame.time.delay(cf.DELAY)
 
     surface.fill(cf.BG_COLOR)
 
@@ -18,7 +23,23 @@ while (True):
         if event.type == pygame.QUIT:
             pygame.quit()
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                snake.changeDirTo(cf.DOWN)
+            
+            elif event.key == pygame.K_RIGHT:
+                snake.changeDirTo(cf.RIGHT)
+            
+            elif event.key == pygame.K_LEFT:
+                snake.changeDirTo(cf.LEFT)
+
+            elif event.key == pygame.K_UP:
+                snake.changeDirTo(cf.UP)
+
     # draw snake
     snake.draw()
+
+    # move snake
+    snake.move()
 
     pygame.display.flip()
--- snake.py
+++ snake.py
@@ -7,7 +7,7 @@ class Snake:
     def __init__(self, surface):
 
         self.pos = [cf.CELL_SIZE * 2, 0]
-        self.snakeBoy = [
+        self.snakeBody = [
             [0, 0],
             [cf.CELL_SIZE, 0],
             [cf.CELL_SIZE * 2, 0]
@@ -17,6 +17,35 @@ class Snake:
         self.surface = surface
 
     def draw(self):
-        for dot in self.snakeBoy:
+        for dot in self.snakeBody:
             d = Dot(self.surface, dot[0], dot[1])
             d.draw()
+
+    def move(self):
+        if self.currentDir == cf.RIGHT:
+            self.pos[0] += cf.CELL_SIZE
+        elif self.currentDir == cf.DOWN:
+            self.pos[1] += cf.CELL_SIZE
+        elif self.currentDir == cf.LEFT:
+            self.pos[0] -= cf.CELL_SIZE
+        elif self.currentDir == cf.UP:
+            self.pos[1] -= cf.CELL_SIZE
+
+        newPos = self.pos.copy()
+        self.snakeBody.append(newPos)
+        self.snakeBody.pop(0)
+
+    def changeDirTo(self, to):
+        if to == cf.RIGHT and self.currentDir != cf.LEFT:
+            self.currentDir = cf.RIGHT
+        
+        elif to == cf.UP and self.currentDir != cf.DOWN:
+            self.currentDir = cf.UP
+
+        elif to == cf.LEFT and self.currentDir != cf.RIGHT:
+            self.currentDir = cf.LEFT
+        
+        elif to == cf.DOWN and self.currentDir != cf.UP:
+            self.currentDir = cf.DOWN
+        
+            
